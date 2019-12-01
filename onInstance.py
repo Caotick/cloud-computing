d8c22bf61329021bb32209d96c2e8652ccddf780
Caotick <43408631+Caotick@users.noreply.github.com>
2019-12-01 20:03:55 +0000

    Update onInstance.py

--- onInstance.py
+++ onInstance.py
@@ -45,8 +45,8 @@ def send_message(queue_url, goldenNonce) :
         return 0
     return 1    
 
-s = boto3.Session(aws_access_key_id='ASIAQCOJS3VZ5MGUPRW6', aws_secret_access_key='zHvIY7WctUZUqkM9Kt0UAGzmJYqxpWzLRC9yAhhN',
-aws_session_token='FwoGZXIvYXdzEMr//////////wEaDG4Q3QxKSdFb6NM+tiLEAZgreLN9jL4VhhVFt1XCc12nyVBEeSo3pDyrYUwhsMeRd7jlRfRRGaXGOLivAyw5t1wmOWprXLB3j5bn47BP6Zc2IsbnN+boZnBiv2ntD9JFC5gZUv5heJtdILC6nAoiYDbzUztSWzmEUqA8HulYfkvlNyMccbuwQ2sNKMW+P+xApFV62/SF/AkuuaNJN9x0C3NkBBSMSSPdHQmCUncfs7lXj0MBDORMw7ZLQV3o3zTESZWFfap+++K/v3wThG8/NCufi/Mond6P7wUyLaXnCZOHsAel30u22duhY+tOGkZUH3SaLqLJbhOxPSOIkekx2cutsCREoC42nA==')
+s = boto3.Session(aws_access_key_id='ASIAQCOJS3VZ3QWX6WPN', aws_secret_access_key='cCIfdEl1SUaKUgMYmuZxek0HlgMpBGAzYFXGVNGW',
+aws_session_token='FwoGZXIvYXdzEM3//////////wEaDKEPDP1JRwSej0abTiLEATOa1k0w9VwfitA2dOB2xd/jw6iVR8q1388V17Od0+QnR8qIMg3/qyPsSa2HwJHMn2dhl1Y9+SbkSe/Rj3nz7928JWd+n4omK9H1i+flpzeFoQykBWEZknkKifAYVFigBqrkyfAKK6Fq4TG1wkoQ3dGEJaSoR/zQhowYU7tpbXLH4F1xn6rtBnhFV1df87RcQgjk6gwN3PJfztvMMlXf42Hb4AI7WYPD5AafaerQ2Tc0b3TQk1KRiUBAERihIDMLYe1hH44o4biQ7wUyLQoVDcE/y0gM6HmubpPsOi6b8sKr+5LBZ5loQ7pl/4iqeMqeX6j+WyngHdyJGg==')
 sqs = s.client('sqs')
 block = "COMSM0010cloud"
 block_bytes = bytes(block)
